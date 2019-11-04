366bd9d5a6eb5ed15125b42bc737a2631c10a8c9
girardb <benjamin.girard.protic@gmail.com>
2019-11-03 19:24:28 -0500

    first?

--- TP3-Q1.py
+++ TP3-Q1.py
@@ -1,83 +1,75 @@
 import socket, optparse, sys
+from socketUtil import recv_msg, send_msg
 from cryptoModule import entierAleatoire, trouverNombrePremier, exponentiationModulaire
 
-from socketUtil import recv_msg, send_msg
+parser = optparse.OptionParser()
+parser.add_option("-s", "--server", action="store_true", dest="serveur", default=False)
+parser.add_option("-a", "--address", action="store", dest="address", default="localhost") #TODO: remplace -a par -d ou c'est différent?
+parser.add_option("-d", "--destination", action="store", dest="destination")
+parser.add_option("-p", "--port", action="store", dest="port", type=int, default=-1)
+opts = parser.parse_args(sys.argv[1:])[0]
 
+if opts.destination and opts.serveur:
+    raise Exception("L'application ne peut pas utiliser -d et -s simultanément.")
 
-# Début du code
-# 1. S'échanger a et b
-# A génère p B génere q
-# 2. Calculer les clés A et B
-# 3. S'échanger A et B
-# 4. Calculer k
-# 
+if opts.port == -1:
+    raise Exception("L'option -p est obligatoire.")
 
-a = trouverNombrePremier()
-b = entierAleatoire(a)
-
-def cle_envoyée(a, b, p_ou_q):
-    A_ou_B = exponentiationModulaire(b, p_ou_q, a)
-    return A_ou_B
-
-def calcul_cle_partage(A_ou_B, p_ou_q, a):
-    k = exponentiationModulaire(A_ou_B, p_ou_q, a)
-    return k
-
-def sequenceA(a, b):
-    p = entierAleatoire(a)
-    A = cle_envoyée(a, b, p)
-    # Envoie de la clé à B
+#TODO: retourner un message d'erreur significatif s'il y a une erreur
+#TODO: chaque message d'erreur (et sa date et heure) doit être écrit dans le fichier Error.log
+if opts.serveur: # mode serveur
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serversocket.bind((opts.address, opts.port))
+    serversocket.listen(5)
     
-    # Reception de la clé B
-    
-    B = None
-
-    k = calcul_cle_partage(B, p, a)
-    return k
-
-def sequenceB(a, b):
-    q = entierAleatoire(a)
-    B = cle_envoyée(a, b, q)
-    # Envoie de la clé à A
-    
-    # Reception de la clé A
-    
-    A = None
-
-    k = calcul_cle_partage(A, q, a)
-    return k
-
-def main():
-    #choisissez l’adresse avec l’option -a et le port avec -p
-    parser = optparse.OptionParser()
-    parser.add_option("-s", "--server", action="store_true", dest="serveur", default=False)
-    parser.add_option("-a", "--address", action="store", dest="address", default="localhost")
-    parser.add_option("-p", "--port", action="store", dest="port", type=int, default=1337)
-    opts = parser.parse_args(sys.argv[1:])[0]
-
-    if opts.serveur: #mode serveur
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        serversocket.bind(("localhost", opts.port))
-        serversocket.listen(5)
+    while True:
+        (s, address) = serversocket.accept()
         
-        while True:
-            (s, address) = serversocket.accept()
-            a = trouverNombrePremier()
-            b = entierAleatoire(a)
-            
+        #create a,b p
+        a = trouverNombrePremier()
+        b = entierAleatoire(a)
+        p = entierAleatoire(a)
 
-            send_msg(s, a+b)#
-            
-            clé_B = recv_msg(s)
-            
-            send_msg(s, "Enchante, " + nom)
-            s.close()
-    else: #mode client
-        destination = (opts.address, opts.port)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(destination)
-        print(recv_msg(s))
-        send_msg(s, input())
-        print(recv_msg(s))
+        #create public key
+        server_public_key = exponentiationModulaire(b, p, a)
+
+        send_msg(s, str(a)) # send a
+        send_msg(s, str(b)) # send b
+        send_msg(s, str(server_public_key)) # send public key
+        
+        # receive client public key
+        client_public_key = int(recv_msg(s))
+
+        #calculate shared key
+        shared_key = exponentiationModulaire(client_public_key, p, a)
+        print(shared_key)
+
         s.close()
+
+else: # mode client
+    destination = (opts.destination, opts.port)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(destination)
+
+    a = int(recv_msg(s)) # receive a
+    b = int(recv_msg(s)) # receive b
+
+    #create q
+    q = entierAleatoire(a)
+
+    #Create public key
+    client_public_key = exponentiationModulaire(b, q, a)
+
+    # receive server public key
+    server_public_key = int(recv_msg(s)) 
+
+    #send public key
+    send_msg(s, str(client_public_key))
+
+    #calculate shared key
+    shared_key = exponentiationModulaire(server_public_key, q, a)
+    print(shared_key)
+
+    s.close()
